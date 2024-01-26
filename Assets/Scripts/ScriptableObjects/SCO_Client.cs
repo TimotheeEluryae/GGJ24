d0cd59d724baf41c8af556d7610de015b61c4e75
Steamcollect <107922742+Steamcollect@users.noreply.github.com>
2024-01-26 19:56:51 +0100

    eliot 2

--- Assets/Scripts/ScriptableObjects/SCO_Client.cs
+++ Assets/Scripts/ScriptableObjects/SCO_Client.cs
@@ -1,16 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Recipe", menuName = "ScriptableObject/Recipe")]
+[CreateAssetMenu(fileName = "Client", menuName = "ScriptableObject/Client")]
 public class SCO_Client : ScriptableObject
 {
     public string name;
-    [TextArea]public List<string> initialTxt = new List<string>();
-    [TextArea]public List<string> exitTxtOK = new List<string>();
-    [TextArea]public List<string> exitTxtNOK = new List<string>();
+    [TextArea]public List<Dialog> initialTxt = new List<Dialog>();
+    [TextArea]public List<Dialog> exitTxtOK = new List<Dialog>();
+    [TextArea]public List<Dialog> exitTxtNOK = new List<Dialog>();
 
     public Sprite spritOK, spriteNOK;
     public Vector2 reputationMinMax;
 
     public List<SCO_Recipe> recipes = new List<SCO_Recipe>();
+}
+
+[System.Serializable]
+public class Dialog
+{
+    public string txt;
 }
--- Assets/Scripts/ScriptableObjects/SCO_Recipe.cs
+++ Assets/Scripts/ScriptableObjects/SCO_Recipe.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
-public class SCO_Recipe : MonoBehaviour
+[CreateAssetMenu(fileName = "Recipe", menuName = "ScriptableObject/Recipe")]
+public class SCO_Recipe : ScriptableObject
 {
+    public string name;
+    [TextArea]public string description;
+
     public Vector2 ingredient1, ingredient2, ingredient3, ingredient4, ingredient5;
 }
